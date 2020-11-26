937b96dca5d7a9c3bdbda80584978395b0b3401b
Guschin Alexandr <igushin1999@gmail.com>
2020-11-26 20:56:54 +0300

    mvn compile

---
+++ src/main/java/com/gusch/app/EchoServer.java
@@ -0,0 +1,23 @@
+package hello;
+import io.grpc.*;
+import grpc.*;
+
+public class EchoServer extends EchoServiceGrpc.EchoServiceImplBase {
+    @Override
+    public void echo(EchoRequest request,
+                     io.grpc.stub.StreamObserver<EchoResponse> responseObserver) {
+        System.out.println("receive:"+request.getMessage());
+        EchoResponse response = EchoResponse.newBuilder().setMessage("hello from server:"+request.getMessage()).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    public static void main(String[] args) throws Exception{
+        Server server = ServerBuilder
+                .forPort(8080)
+                .addService(new EchoServer()).build();
+        server.start();
+        System.out.println("Server started");
+        server.awaitTermination();
+    }
+}
