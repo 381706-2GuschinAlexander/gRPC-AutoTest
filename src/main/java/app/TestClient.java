330328a84ca34ccec7dc00efa0339b267e775d48
Guschin Alexandr <igushin1999@gmail.com>
2020-11-28 19:50:26 +0300

    Add test_name

--- src/main/java/app/TestClient.java
+++ src/main/java/app/TestClient.java
@@ -4,16 +4,32 @@ import io.grpc.*;
 import java.util.Scanner;
 
 public class TestClient {
+    Integer local_id = -1;
+
     public static void main(String[] args) {
         EchoServiceGrpc.EchoServiceBlockingStub client = createClient("localhost",8080);
         System.out.println("Connected to server");
         Scanner console = new Scanner(System.in);
         String message;
         while((message = console.nextLine())!=null){
-            EchoRequest request = EchoRequest.newBuilder().setMessage(message).build();
-            EchoResponse response = client.echo(request);
-            System.out.println("response="+response.getMessage());
+            requestForm(client, message);
         }
+        console.close();
+    }
+
+    private static int requestForm(EchoServiceGrpc.EchoServiceBlockingStub client, String message){
+        String[] req = message.trim().split(" ");
+
+        if(req[0].equals("insert")){
+            AddIdRequest request = AddIdRequest.newBuilder().setId(0).setName(req[1]).build();
+            EchoResponse response = client.insert(request);
+            System.out.println("response: "+response.getMessage());
+        } else {
+            EchoRequest request = EchoRequest.newBuilder().setMessage("aaa").build();
+            EchoResponse response = client.echo(request);
+            System.out.println("response: "+response.getMessage());
+        }
+        return 0;
     }
 
     private static EchoServiceGrpc.EchoServiceBlockingStub createClient(String host, int port){
--- src/main/java/app/TestServer.java
+++ src/main/java/app/TestServer.java
@@ -1,13 +1,40 @@
 package autotest;
+
+import java.util.ArrayList;
 import io.grpc.*;
 import grpc.*;
 
 public class TestServer extends EchoServiceGrpc.EchoServiceImplBase {
+    String log = "";
+    ArrayList<Integer> active_test_id;
+
+    TestServer(){
+        active_test_id = new ArrayList<Integer>();
+    }
+
     @Override
-    public void echo(EchoRequest request,
-                     io.grpc.stub.StreamObserver<EchoResponse> responseObserver) {
+    public void echo(EchoRequest request, io.grpc.stub.StreamObserver<EchoResponse> responseObserver) {
         System.out.println("receive:"+request.getMessage());
         EchoResponse response = EchoResponse.newBuilder().setMessage("hello from server:"+request.getMessage()).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void insert(AddIdRequest request, io.grpc.stub.StreamObserver<EchoResponse> responseObserver){
+        String response_str = "Added new Test";
+        int client_id = request.getId();
+        if(client_id != 0)
+            response_str = "Access denied";
+        else{
+            // if there are alredy test with this name
+            ;
+        }
+        log += request.getName() + "\n";
+        
+        System.out.println(log);
+
+        EchoResponse response = EchoResponse.newBuilder().setMessage(response_str).build();
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
